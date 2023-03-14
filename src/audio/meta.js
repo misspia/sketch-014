ee86301380c8cb9c0a24961ae66cce3079547afb
misspia <pia_leung@hotmail.com>
2023-03-14 23:56:47 +0000

    import sketch

---
+++ src/audio/meta.js
@@ -0,0 +1,6 @@
+export const BASE_URL = 'https://raw.githubusercontent.com/misspia/assets/master/audio/';
+
+export const audioMeta = {
+  url: `${BASE_URL}coven-theme-riot.mp3`,
+  title: `Coven theme - Riot Games x Andrea Bellucci`,
+}
